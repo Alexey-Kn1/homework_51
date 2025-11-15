fe4317f82b6e32cee12a3f7839d02d9bae5eef95
Aleksei <knysh.aleksey@yandex.ru>
2025-11-15 15:42:56 +0300

    StoredCardPresentation implemented as record

--- src/main/java/ru/netology/repository/StoredCardPresentation.java
+++ src/main/java/ru/netology/repository/StoredCardPresentation.java
@@ -3,20 +3,5 @@ package ru.netology.repository;
 import ru.netology.model.BankCard;
 import ru.netology.model.MoneyQuantity;
 
-public class StoredCardPresentation {
-    private BankCard card;
-    private MoneyQuantity balance;
-
-    public StoredCardPresentation(BankCard card, MoneyQuantity balance) {
-        this.card = card;
-        this.balance = balance;
-    }
-
-    public BankCard getCard() {
-        return card;
-    }
-
-    public MoneyQuantity getBalance() {
-        return balance;
-    }
+public record StoredCardPresentation(BankCard card, MoneyQuantity balance) {
 }
